2a90533520983bba86546f801081d76b1c9c683a
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-26 17:07:01 +0500

    Add files via upload

---
+++ object oriented programming/oop/Demo2nd.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Demo2nd
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        int num = input.nextInt();
+
+        if(num > 0)
+            System.out.println(num + " is positive");
+        else if(num <0 )
+            System.out.println(num + " is negative");
+        else
+            System.out.println(num + " is zero");
+    }
+}
