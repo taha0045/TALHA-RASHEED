12f54ec9bdf329a8976dbf83ccec314872b73312
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-26 17:08:15 +0500

    Add files via upload

---
+++ object oriented programming/oop/demo4th.java
@@ -0,0 +1,10 @@
+public class demo4th
+ {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.print("Hello");
+        System.out.print("taha");
+    }
+
+}
