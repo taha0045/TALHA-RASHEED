040949e4b2a72c4ea9030cbee2da68e31d934338
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 09:52:26 +0500

    Create (Main.java

---
+++ oop class/guaid5/Overriding in Java >/(Main.java
@@ -0,0 +1,10 @@
+public class Main{
+public static void main(String[] args){
+Animal a = new Animal();
+Animal b = new Cat();
+// Call method in Animal class
+a.name();
+ // Call method in Cat class
+b.name();
+}
+}
