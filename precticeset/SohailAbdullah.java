55670eecf9ad64a5950d2f3ef0834ba49d0fc43f
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 11:33:07 +0500

    Add files via upload

---
+++ precticeset/SohailAbdullah.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SohailAbdullah {
+    public static void main(String[] args) {
+
+        //System.out.println(" bil trteeb a,b,ckoj v nai haga kaka g sab koj khoood hi ana pane wa input kar salya ");
+        //Scanner sc= new Scanner(System.in );
+       // int a= sc.nextInt();
+       // int b= sc.nextInt();
+       // int c= sc.nextInt();
+
+       // int sum;
+       // sum=a+b+c;
+       // System.out.println(sum);
+        //km to m ...................................
+        System.out.println("enter the km");
+        Scanner km = new Scanner(System.in);
+        float Km = km.nextFloat();
+        float meter = Km*1000f;
+        System.out.println(meter);
+
+    }
+}
