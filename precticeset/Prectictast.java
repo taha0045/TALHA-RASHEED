44f0e1a4e67bf4ad00599ca79d0df1958883c80d
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 11:33:36 +0500

    Add files via upload

---
+++ precticeset/Prectictast.java
@@ -0,0 +1,35 @@
+package JavaClasswork.harry;
+import java.util.Scanner ;
+public class Prectictast {
+    public static void main(String[] args) {
+       //    #1st QUESTION   fake grade................
+
+          // char grade = 'a';
+
+         //grade = (char) (grade+8);
+         //System.out.println(grade);
+        //orignal grade...............
+
+         //grade = (char) (grade-8);
+         //System.out.println(grade);
+
+        // #2ND   QUESTION......................................
+      //double a = 7/4.0 * 9/2.0;
+        //System.out.println(a);
+
+        //  3rd question.........................................
+        System.out.println("input the value of a");
+      Scanner sc= new Scanner(System.in);
+       int a = sc.nextInt();
+      System.out.println(a>9);
+
+        // #4th  question .........................................
+
+       //System.out.println("(u*u-v*v/2as");
+
+        // #5th  question .........................................
+       //int a= 7*49/7+35/7;
+      // System.out.println(a);
+
+    }
+}
