ddc3dc943cdda7458c5c30c01439ed27e10288a6
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 09:30:49 +0500

    Update and rename 1st to (Item.java

---
+++ oop class/guaide 3/(Item.java
@@ -0,0 +1,24 @@
+public class Item {
+ private String name;
+ private int price;
+ private String type;
+ public Item(String name, int price, String type) {
+ this.name = name;
+ this.price = price;
+ this.type = type;
+ }
+ public String getName() {
+ return name;
+ }
+ public int getPrice() {
+ return price;
+ }
+ public String getType() {
+ return type;
+ }
+ public void showDetail() {
+ System.out.println("- " + getName() + " (" + getType() 
++ " ) - Rp " + getPrice());
+ }
+}
+
