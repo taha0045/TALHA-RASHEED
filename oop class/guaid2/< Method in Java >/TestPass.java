6762febb91139371c507afc85253281941162f88
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 09:25:54 +0500

    Create TestPass.java

---
+++ oop class/guaid2/< Method in Java >/TestPass.java
@@ -0,0 +1,18 @@
+public class TestPass {
+ int number1, number2;
+ // Constructor
+ public TestPass(int number1, int number2) {
+ this.number1 = number1;
+ this.number2 = number2;
+ }
+ // Passed by value
+ void calculate(int m, int n) {
+ m = m * 10;
+ n = n / 2;
+ }
+ // Passed by reference
+ void calculate(TestPass tp) {
+ tp.number1 = tp.number1 * 10;
+ tp.number2 = tp.number2 / 2;
+ }
+}
