fc5b043b64e98cded9296e0447ded9ce63cda73c
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 09:41:18 +0500

    Create Animal.java

---
+++ oop class/guaid4b./Animal.java
@@ -0,0 +1,21 @@
+public class Animal {
+ private String name;
+ public Animal(String name){
+ this.name = name;
+ }
+ public String getName(){
+ return name;
+ }
+ public String getType(){
+ return "Unknown Animal";
+ }
+ public String playSound(){
+ return "Error : Unknown animal type!";
+ }
+ public void showProfile(){
+ System.out.println("Name: " + getName());
+ System.out.println("Type: " + getType());
+ System.out.println("Sound: " + playSound());
+ System.out.println();
+ }
+}
