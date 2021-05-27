c04fcf1eb2cd7a41d2a9a2187d89a9d12c3d8c72
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 10:57:28 +0500

    Create Omnivore.java

---
+++ oop class/unguaid4/Omnivore.java
@@ -0,0 +1,10 @@
+public class Omnivore extends Animal {
+
+    public Omnivore(String characteristics) {
+        super(characteristics);
+    }
+
+    public String getName() {
+        return "OMNIVORE";
+    }
+}
