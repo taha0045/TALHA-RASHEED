db24dd2b42d4aeb8a914f5fa9d8795e803c674de
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 10:54:02 +0500

    Create  Carnivore.java

---
+++ oop class/unguaid4/Carnivore.java
@@ -0,0 +1,11 @@
+ublic class Carnivore extends Animal {
+
+    public Carnivore(String characteristics) {
+        super(characteristics);
+    }
+
+    public String getName() {
+        return "CARNIVORE";
+    }
+
+}
