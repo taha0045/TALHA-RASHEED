7bda8855f956fc3dc1ef30a79f21e15e6fcb0bbf
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 11:32:42 +0500

    Add files via upload

---
+++ precticeset/IttpJavaClasswork.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class IttpJavaClasswork {
+    public static void main(String[] args) {
+    //the sting used...............
+
+      // String namecallme;
+      // namecallme = "talha rasheed";
+      // System.out.println("the student name is :  " + namecallme);
+
+      // the prientf function ............
+
+      // int a= 15;
+      // float b= 7.0020f;
+       // System.out.printf("the ratio of a %1d the ration of b %11.2f ",a,b);
+
+       Scanner sc=new Scanner(System.in);
+
+       System.out.println("enter u r name");
+       // String name = sc.next();    //the only frist one word
+      String name = sc.nextLine();  // the accsicute the whole line
+        System.out.println(" the studend name "+ name);
+
+
+        //String name = "talha rasheed";
+       // System.out.println(name);
+       /* int value = name.length();       // lenght of string
+        System.out.println(value);
+        String lstring = name.toLowerCase();      // toLowerCase  of string
+        System.out.println(lstring);
+        String ustring = name.toUpperCase();        //toUpperCase    of string
+        System.out.println(ustring);
+        String nonTrimmedString = "     talha rasheed     ";   //  nonTrimmedString   of string
+        System.out.println(nonTrimmedString);
+        String trimmedString = nonTrimmedString.trim();       //  nonTrimmedString.trim
+        System.out.println(trimmedString);
+        System.out.println(name.substring(5));                 // start substring
+        System.out.println(name.substring(0,10));               // start and end sub string
+
+        System.out.println(name.replace('r', 'p'));    // replace word
+        System.out.println(name.replace("r", "ier"));              // replace more word with one word
+
+        System.out.println(name.startsWith("t"));     //startsWith  sring find chak tru are falsa
+        System.out.println(name.endsWith("d"));           //endsWith   string find chak true or false
+
+        System.out.println(name.charAt(7));   // write the crecter of the number space exp. 7 num is 'r' so r is print
+
+
+        String modifiedName = "harryrryrry";
+        System.out.println(modifiedName.indexOf("e"));
+        System.out.println(modifiedName.indexOf("rry", 4));
+        System.out.println(modifiedName.lastIndexOf("rry", 7));
+        */
+      // System.out.println(name.equals("talha rasheed"));
+        //System.out.println(name.equalsIgnoreCase("talha sheed")); // chak name right or fulase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
