dcd795acfb80ac06d4128fd5306802b57a8c8ad7
taha0045 <84560411+taha0045@users.noreply.github.com>
2021-05-27 09:40:12 +0500

    Create Rabbit.java

---
+++ oop class/guaid4b./Rabbit.java
@@ -0,0 +1,11 @@
+public class Rabbit extends Animal {
+ public Rabbit(String name) {
+ super(name);
+ }
+ public String getType() {
+ return "Rabbit";
+ }
+ public String playSound() {
+ return "Squeak~!";
+ }
+}
